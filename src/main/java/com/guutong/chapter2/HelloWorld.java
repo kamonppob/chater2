00d7967726f01b2b7e80e8e25956941ccb693344
GuutonG <GuutonG@GuutonG-NB>
2014-12-21 10:13:40 +0700

    first commit step learnning2

--- src/main/java/com/guutong/chapter2/HelloWorld.java
+++ src/main/java/com/guutong/chapter2/HelloWorld.java
@@ -11,6 +11,6 @@ package com.guutong.chapter2;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld"); 
+        System.out.println("HelloWorld 2"); 
     }
 }
