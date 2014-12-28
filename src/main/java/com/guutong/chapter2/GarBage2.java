3fe6a9a0c687ff8bb0e54c68dbc6c474b17478fd
GuutonG <GuutonG@GuutonG-NB>
2014-12-28 10:21:53 +0700

    SUCCESS type show even

---
+++ src/main/java/com/guutong/chapter2/GarBage2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.guutong.chapter2;
+
+/**
+ *
+ * @author GuutonG
+ */
+public class GarBage2 {
+    public static void main(String[] args) {
+        Object Obj = new Object();
+       
+     
+        
+        
+    }
+    
+}
